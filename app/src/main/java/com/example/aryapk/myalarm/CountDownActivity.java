558f3ba58be2a032397f0464e66fb370862d428e
Farhan Ramadhana <farhanramadhana97@gmail.com>
2017-12-07 13:51:06 +0700

    add countdown fitur

---
+++ app/src/main/java/com/example/aryapk/myalarm/CountDownActivity.java
@@ -0,0 +1,13 @@
+package com.example.aryapk.myalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountDownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count_down);
+    }
+}
