c70440fec283fdb3c5bce8011c01b5efee985e05
Farhan Ramadhana <farhanramadhana97@gmail.com>
2017-12-12 16:28:57 +0700

    initialize linear layout

--- app/src/main/java/com/example/aryapk/myalarm/Fragment/CountDownTimerFragment.java
+++ app/src/main/java/com/example/aryapk/myalarm/Fragment/CountDownTimerFragment.java
@@ -5,19 +5,31 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.NotificationCompatApi24;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.aryapk.myalarm.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class CountDownTimerFragment extends Fragment {
-    com.shawnlin.numberpicker.NumberPicker clockPicker, minutePicker, secondPicker ;
-    Button reset, start;
+    @Bind(R.id.clock_picker) com.shawnlin.numberpicker.NumberPicker clockPicker;
+    @Bind(R.id.minute_picker) com.shawnlin.numberpicker.NumberPicker minutePicker;
+    @Bind(R.id.second_picker) com.shawnlin.numberpicker.NumberPicker secondPicker;
+    @Bind(R.id.tvCountDownView) TextView tvCountDownView;
+    @Bind(R.id.btnReset) Button reset;
+    @Bind(R.id.btnStart) Button start;
+    @Bind(R.id.llTimePicker)
+    LinearLayout llTimePicker;
+
     long timeLeft = 0;
     boolean timerRunning;
+    CountDownTimer countDownTimer;
     View v;
 
     public CountDownTimerFragment() {
@@ -33,13 +45,8 @@ public class CountDownTimerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         v =  inflater.inflate(R.layout.fragment_count_down_timer, container, false);
-        // find picker id
-        clockPicker= (com.shawnlin.numberpicker.NumberPicker) v.findViewById(R.id.clock_picker);
-        minutePicker= (com.shawnlin.numberpicker.NumberPicker) v.findViewById(R.id.minute_picker);
-        secondPicker = (com.shawnlin.numberpicker.NumberPicker) v.findViewById(R.id.second_picker);
-        //find button id
-        reset = (Button) v.findViewById(R.id.btnReset);
-        start = (Button) v.findViewById(R.id.btnStart);
+        ButterKnife.bind(this,v);
+
         //clock picker setting
         clockPicker.setMinValue(0);
         clockPicker.setMaxValue(99);
@@ -57,8 +64,10 @@ public class CountDownTimerFragment extends Fragment {
         start.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                int timeInput  = Integer.parseInt(clockPicker.getDisplayedValues().toString());
-                timeLeft = (long) timeInput * 60000;
+                int clockInput  = Integer.parseInt(clockPicker.getDisplayedValues().toString());
+                int minuteInput  = Integer.parseInt(minutePicker.getDisplayedValues().toString());
+                int secondInput  = Integer.parseInt(secondPicker.getDisplayedValues().toString());
+                timeLeft = (long) clockInput * 3600000 +  (long) minuteInput * 60000 + (long) secondInput * 1000;
                 startStop(timeLeft);
             }
         });
@@ -75,14 +84,44 @@ public class CountDownTimerFragment extends Fragment {
     }
 
     private void stopTimer() {
+        countDownTimer.cancel();
+        start.setText("START");
+        timerRunning = false;
     }
 
     private void startTimer(){
+        tvCountDownView.setVisibility(View.VISIBLE);
+        llTimePicker.setVisibility(View.GONE);
+        countDownTimer = new CountDownTimer(timeLeft, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeft = l;
+                updateTimer();
+            }
 
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        start.setText("PAUSE");
+        timerRunning = true;
     }
 
-    private void updateTimer(){
+    private void updateTimer() {
+        int clock = (int) timeLeft / 360000;
+        int minutes = (int) timeLeft / 60000;
+        int seconds = (int) timeLeft % 60000 / 1000;
+        String timeLeftText;
+        timeLeftText = ""+clock;
+        timeLeftText += ":";
+        timeLeftText = ""+minutes;
+        timeLeftText += ":";
+        if (seconds < 10)
+            timeLeftText +=0;
+        timeLeftText += seconds;
 
+        tvCountDownView.setText(timeLeftText);
     }
 
     @Override
