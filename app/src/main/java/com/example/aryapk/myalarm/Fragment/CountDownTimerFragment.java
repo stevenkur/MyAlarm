607d4dd6e02bf10503ecae803d568c4a2f1db8af
Farhan Ramadhana <farhanramadhana97@gmail.com>
2017-12-12 15:25:30 +0700

    fix error on .xml and update initialize variable on .java

--- app/src/main/java/com/example/aryapk/myalarm/Fragment/CountDownTimerFragment.java
+++ app/src/main/java/com/example/aryapk/myalarm/Fragment/CountDownTimerFragment.java
@@ -8,16 +8,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.NumberPicker;
-import android.widget.TextView;
 
 import com.example.aryapk.myalarm.R;
 
 public class CountDownTimerFragment extends Fragment {
-
     com.shawnlin.numberpicker.NumberPicker clockPicker, minutePicker, secondPicker ;
+
     View v;
     public CountDownTimerFragment() {
     }
@@ -31,7 +27,22 @@ public class CountDownTimerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         v =  inflater.inflate(R.layout.fragment_count_down_timer, container, false);
-
+        // find picker id
+        clockPicker= (com.shawnlin.numberpicker.NumberPicker) v.findViewById(R.id.clock_picker);
+        minutePicker= (com.shawnlin.numberpicker.NumberPicker) v.findViewById(R.id.minute_picker);
+        secondPicker = (com.shawnlin.numberpicker.NumberPicker) v.findViewById(R.id.second_picker);
+        //clock picker setting
+        clockPicker.setMinValue(0);
+        clockPicker.setMaxValue(99);
+        clockPicker.setWrapSelectorWheel(true);
+        //minute picker setting
+        minutePicker.setMinValue(0);
+        minutePicker.setMaxValue(59);
+        minutePicker.setWrapSelectorWheel(true);
+        //second picker setting
+        secondPicker.setMinValue(0);
+        secondPicker.setMaxValue(59);
+        secondPicker.setWrapSelectorWheel(true);
         return v;
     }
 
