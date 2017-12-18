a4048e3e452db0cf59b5439e541b6e667f0c15f7
Farhan Ramadhana <farhanramadhana97@gmail.com>
2017-12-18 21:35:01 +0700

    add stop and reset class

--- app/src/main/java/com/example/aryapk/myalarm/countdown/CountDownTimerResetState.java
+++ app/src/main/java/com/example/aryapk/myalarm/countdown/CountDownTimerResetState.java
@@ -6,7 +6,7 @@ import android.content.Context;
  * Created by Farhan Ramadhana on 12/18/2017.
  */
 
-public class CountDownTimerStartState implements CountDownTimerState {
+public class CountDownTimerResetState implements CountDownTimerState {
     @Override
     public void doAction(Context context) {
 
--- app/src/main/java/com/example/aryapk/myalarm/countdown/CountDownTimerState.java
+++ app/src/main/java/com/example/aryapk/myalarm/countdown/CountDownTimerState.java
@@ -1,8 +1,11 @@
 package com.example.aryapk.myalarm.countdown;
 
+import android.content.Context;
+
 /**
  * Created by Farhan Ramadhana on 12/18/2017.
  */
 
 public interface CountDownTimerState {
+    public void doAction(Context context);
 }
